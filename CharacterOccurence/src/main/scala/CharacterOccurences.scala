d2be8b55fadba7aeb8485688a96e528ad51ada69
David Melcher <dm070491@fh-muenster.de>
2018-03-26 15:05:23 +0200

    Added character occurence example

---
+++ CharacterOccurence/src/main/scala/CharacterOccurences.scala
@@ -0,0 +1,11 @@
+object CharacterOccurences {
+
+  def main(args : Array[String]): Unit = {
+
+    val text =  if (args.length > 0) args(0) else "MISSISSIPI"
+
+    val result = text.toCharArray().sortBy(_.charValue()).groupBy(_.charValue()).map(x => (x._1, x._2.length))
+
+    println(result)
+  }
+}
